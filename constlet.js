cac6dd7c8ae13ff5d7b0777c2eb6f5616503d8b0
zueunjoeng <judms0314@gmail.com>
2024-05-27 11:21:42 +0900

    두번째 방법

--- constlet.js
+++ constlet.js
@@ -1,7 +1,3 @@
 let study="프론트엔드";
-// 프론트엔드가 변수네~ 얘는 ""안으로 들어가면 인식을 못해
 const myword="그래 나도 한번 프론트엔드 해보자!" ;
 console.log(myword);
-
-// +const myword="그래 나도 한번 프론트엔드 해보자!" ;
-// console.log(myword);
