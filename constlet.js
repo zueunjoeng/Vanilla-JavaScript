c01f6f1ff583240252bd2f1589ad4b8a25c4ccb0
zueunjoeng <judms0314@gmail.com>
2024-05-27 11:37:18 +0900

    3번 복습

--- constlet.js
+++ constlet.js
@@ -1,3 +1,7 @@
-let study="프론트엔드";
-const myword=`그래 나도 한번 ${study} 해보자!`; //백틱 붙일것**
+let _month=6;
+let _study='피그마'
+let _name='홍길동'
+// ''경우 ; 신경쓰지않음.
+const myword=`저는 ${_month}개월간 ${_study} 공부를 한 ${_name}입니다.`;
+// +언더바를 자주 사용함. 
 console.log(myword);
