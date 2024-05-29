d9b6ac971eca1016f35d1ec7f4f06aad896caf82
zueunjoeng <judms0314@gmail.com>
2024-05-29 12:43:04 +0900

    후치연산

--- arr_for.js
+++ arr_for.js
@@ -1,18 +1,27 @@
-const nav_data=[
-    {
-        gnb_li_a: "회사소개",
-        gnb_li_href:"./company.html"
-    },
+console.log("한번만 실행되지");
 
-    {
-        gnb_li_a: "제품소개",
-        gnb_li_href:"./product.html"
-    },
+//초기값을 읽고 조건을 읽고 참이라면 실행하고 연산식 실행, 아직도 참이라면 실행하고 연산식 실행
+//그러다가 조건이 거직이 되는 순간 for문 종료
 
-    {
-        gnb_li_a: "커뮤니티",
-        gnb_li_href:"./board.html"
-    }
-]
+for(var i = 0; i < 5; i++ ){
+    console.log("나는 다섯번 실행되지");
+}
 
-console.log(nav_data[2].gnb_li_a)
+// let count=10;
+// count++; // count +=1 : 1을 추가로 넣는다 <-> ++count 
+// count++; //다음연산에 1씩 추가증가하겠다. (그래서 여기는 10)
+// count; // (그럼 얘는 11)
+// ++count;// 얘는 12. ++가 앞에 붙으면 지금의 나에게
+//count++ =>12 맞아.
+//count +=1 // 얘는 12에 1이 추가된거라 13이지
+//지금의 나에게는 상관없고 다음의 나에게 전달하는 => 후치연산
+
+let count =10;
+    count +=100;
+    count +=1; //111
+    ++count; //112
+    count++; //112
+
+    console.log(count); //113
+
+
