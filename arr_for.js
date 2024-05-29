daa59fe6377b1fee8a08655999459bff32919b3f
zueunjoeng <judms0314@gmail.com>
2024-05-29 12:13:34 +0900

    html에 자바스크립트 적용-오타주의

---
+++ arr_for.js
@@ -0,0 +1,18 @@
+const nav_data=[
+    {
+        gnb_li_a: "회사소개",
+        gnb_li_href:"./company.html"
+    },
+
+    {
+        gnb_li_a: "제품소개",
+        gnb_li_href:"./product.html"
+    },
+
+    {
+        gnb_li_a: "커뮤니티",
+        gnb_li_href:"./board.html"
+    }
+]
+
+console.log(nav_data[2].gnb_li_a)
